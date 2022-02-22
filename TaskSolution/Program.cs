e35eec103219dcbc5b218efa3ac037354bd92973
Konstantin Stepanov <688stakan@gmail.com>
2022-02-23 00:22:37 +0300

    method for printint arrays created

--- TaskSolution/Program.cs
+++ TaskSolution/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма. При решении не рекомендуется пальзоваться коллекциями,
+лучше обойтись исключительно массивами.*/ 
+
+string[] StartArray = {"hello", "2", "world", ":-)"};
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"\"{array[i]}\"");
+        if (i != array.Length - 1)
+            Console.Write(", ");
+    }
+    Console.Write("]");
+}
+PrintArray(StartArray);
