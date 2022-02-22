a9ac315503fef563bfec1eace94dbe8e14cd7492
Konstantin Stepanov <688stakan@gmail.com>
2022-02-23 01:27:54 +0300

    code is ready for review

--- TaskSolution/Program.cs
+++ TaskSolution/Program.cs
@@ -3,7 +3,8 @@
 либо задать на старте выполнения алгоритма. При решении не рекомендуется пальзоваться коллекциями,
 лучше обойтись исключительно массивами.*/ 
 
-string[] StartArray = {"hello", "2", "world", ":-)"};
+//string[] StartArray = {"hello", "2", "world", ":-)"};
+string[] StartArray = {"Russia", "Kazan", "Denmark"};
 
 void PrintArray(string[] array)
 {
@@ -28,19 +29,23 @@ int NewArraySize(string[] array)
     return numberForSize;
 }
 
-string[] NewArray(string[] array, int NewArraySize)
+string[] CreateNewArray(string[] array, int newArraySize)
 {
-    string[] NewArray = new string[NewArraySize];
+    string[] NewArray = new string[newArraySize];
     int j = 0;
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length < 4)
+        {
             NewArray[j] = array[i];
             j++;
+        }
     }
     return NewArray;
 }
 
-
-
 PrintArray(StartArray);
+//int newArraySize = NewArraySize(StartArray);
+string[] newArray = CreateNewArray(StartArray, NewArraySize(StartArray));
+Console.Write("->");
+PrintArray(newArray);
